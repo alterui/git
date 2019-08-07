8fb9b43c57bb68de6c654f648463c31012faa30e
liurui <liu.rui@oyohotels.cn>
2019-08-07 20:40:25 +0800

    new class b1

---
+++ src/B1.java
@@ -0,0 +1,12 @@
+import javax.crypto.spec.PSource;
+
+/**
+ * @author liurui
+ * @time 2019-08-07 20:39
+ */
+public class B1 {
+    public static void main(String[] args) {
+        System.out.println("this is B1");
+
+    }
+}
