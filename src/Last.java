34daa62e462613c7373b3a51257085c28141c01a
liurui <liu.rui@oyohotels.cn>
2019-08-07 21:15:20 +0800

    new class DDD and Last

---
+++ src/Last.java
@@ -0,0 +1,10 @@
+/**
+ * @author liurui
+ * @time 2019-08-07 21:06
+ */
+public class Last {
+    public static void main(String[] args) {
+        System.out.println("this is last demo");
+
+    }
+}
